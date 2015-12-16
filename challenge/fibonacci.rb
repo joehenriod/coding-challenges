8d76d27688bfafd3dbe3db1fa00f30df02a47a14
Joe Henriod <joehenriod@gmail.com>
2015-12-16 23:39:33 +0000

    why isn't the loop iterating

--- challenge/fibonacci.rb
+++ challenge/fibonacci.rb
@@ -19,13 +19,19 @@ end
 #recursive_fib(10)
 
 
-def interative_fib(n)
-  starting_array = [0,1]
+def iterative_fib(n)
+  
+  n.times do 
+    fib_array = [0,1]
 
-  first_operation = starting_array.reverse
-  second_operation = first_operation[0] + first_operation[1]
-  puts second_operation
+    first_operation = fib_array.reverse
+    second_operation = first_operation[0] + first_operation[1]
+    fib_array << second_operation.to_i
+
+
+    puts fib_array
+  end
 
 end
 
-interative_fib(1)
+iterative_fib(3)

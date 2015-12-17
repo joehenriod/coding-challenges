fe442e0746c3fdb0d5d3fdd42190fb7a359e4477
Joe Henriod <joehenriod@gmail.com>
2015-12-17 19:23:10 +0000

    solved for 1

--- challenge/collatz.rb
+++ challenge/collatz.rb
@@ -1,2 +1,17 @@
-n → n/2 (n is even)
-n → 3n + 1 (n is odd)
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+
+
+
+def collatz(n)
+
+coll_arr = []
+
+  if n == 1
+    coll_arr << n 
+  end
+
+end
+
+
+puts collatz(1)

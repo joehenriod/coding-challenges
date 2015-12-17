a2e828ebe5ab229270ee23c38fcfd830d658d118
Joe Henriod <joehenriod@gmail.com>
2015-12-17 19:12:57 +0000

    create collatz file

---
+++ challenge/collatz.rb
@@ -0,0 +1,2 @@
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
--- chess-app/db/schema.rb
+++ chess-app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213135958) do
+ActiveRecord::Schema.define(version: 20151215064111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151213135958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "game_id"
+    t.boolean  "is_white"
   end
 
   create_table "users", force: :cascade do |t|

4ca6dda2bdfe5704a16aec5c8f51801cc361f22e
Joe Henriod <joehenriod@gmail.com>
2015-12-20 22:31:58 +0000

    still not getting the function to work inside of a function

--- challenge/collatz.rb
+++ challenge/collatz.rb
@@ -3,33 +3,33 @@
 
 
 
-def collatz(n)
-
-coll_arr = []
-
-  while coll_arr.last != 1
-    if n.even?
-      coll_arr << (n/2)
-    elsif n.odd?
-      coll_arr << ((n * 3) + 1)
-    end
-    n = coll_arr.last
-  end
-  
-  coll_arr.length
-
-end
 
 
 
 def counter
+
+  def collatz(n)
+  coll_arr = []
+    while coll_arr.last != 1
+      if n.even?
+        coll_arr << (n/2)
+      elsif n.odd?
+        coll_arr << ((n * 3) + 1)
+      end
+      n = coll_arr.last
+    end
+    coll_arr.length
+   end
+
+
+
   num = (1..100).to_a
 
-  num.each do |n|
-
+  num.each do |x|
+    collatz(x)
   end
+
 end
 
-n = (1..100).to_a
 
-puts collatz(n)
+puts counter

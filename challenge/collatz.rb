324a33bc981cf985b5940b967fae85949fcb7c29
Joe Henriod <joehenriod@gmail.com>
2015-12-23 23:55:01 +0000

    The collatz is working

--- challenge/collatz.rb
+++ challenge/collatz.rb
@@ -2,30 +2,33 @@
 # n → 3n + 1 (n is odd)
 
 #Why can't I figure out how to get a method inside another method?
-def counter
 
-  def collatz(n)
+def collatz(n)
   coll_arr = []
-    while coll_arr.last != 1
-      if n.even?
-        coll_arr << (n/2)
-      elsif n.odd?
-        coll_arr << ((n * 3) + 1)
-      end
-      n = coll_arr.last
+  while coll_arr.last != 1
+    if n.even?
+      coll_arr << (n/2)
+    elsif n.odd?
+      coll_arr << ((n * 3) + 1)
     end
-    coll_arr.length
-   end
+    n = coll_arr.last
+  end
+  coll_arr.length
+end
 
 
 
-  num = (1..100).to_a
+def counter
+  num = (1..1000000).to_a
+  calculated_values = []
 
-  num.each do |x|
-    collatz(x)
+  num.each do |n|
+   calculated_values << collatz(n)
   end
 
+return calculated_values.max
 end
 
 
 puts counter
+

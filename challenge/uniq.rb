2c8c03771531ef71cf54e6192f17f81530168e21
Joe Henriod <joehenriod@gmail.com>
2015-12-30 06:51:15 +0000

    finished removed duplicates method without uniq

--- challenge/uniq.rb
+++ challenge/uniq.rb
@@ -1,8 +1,13 @@
 array_with_duplicates = [1,2,2,2,3,4,5,5,5,6,7]
 cleaned_array = []
+garbage = []
 
 array_with_duplicates.each do |x|
-  cleaned_array << x
+  if cleaned_array.include?(x)
+    garbage << x
+  else
+    cleaned_array << x
+  end
 end
 
-
+print cleaned_array, garbage

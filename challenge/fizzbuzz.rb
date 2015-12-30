709f771eb108290820be6dba003c910788f255f4
Joe Henriod <joehenriod@gmail.com>
2015-12-30 19:44:36 +0000

    edge case 0

--- challenge/fizzbuzz.rb
+++ challenge/fizzbuzz.rb
@@ -1,6 +1,9 @@
 def fizzbuzz
   (-50).upto(100) do |x|
-      if x % 3 == 0 && x % 5 == 0
+      if x == 0
+        puts 0
+      elsif 
+       x % 3 == 0 && x % 5 == 0
         puts "FizzBuzz"
       elsif x % 3 == 0
         puts "Fizz"
@@ -9,7 +12,7 @@ def fizzbuzz
       else
         puts x
       end
-    end
+  end
 end
 
 puts fizzbuzz

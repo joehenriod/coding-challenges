589011d6ecd84c79fd6800b919ce3d6307c23aec
Joe Henriod <joehenriod@gmail.com>
2016-01-12 00:36:47 +0000

    got it working

--- challenge/rot13.rb
+++ challenge/rot13.rb
@@ -1,2 +1,12 @@
 # Why did the chicken cross the road?
 # Gb trg gb gur bgure fvqr!
+
+
+def rot13(n)
+  return n.tr("abcdefghijklmnopqrstuvwxyz","nopqrstuvwxyzabcdefghijklm")
+end
+
+puts rot13("joe")
+
+
+puts rot13("wbra")

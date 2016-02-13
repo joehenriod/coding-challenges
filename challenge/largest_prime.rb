9a1405229338523af01f9c4dd701953cdf32b503
joehenriod <joehenriod@gmail.com>
2016-02-13 15:12:09 -0800

    Funtion for finding largest prime number

---
+++ challenge/largest_prime.rb
@@ -0,0 +1,18 @@
+#Find the largest Prime
+
+def lPrimeFactor(n)
+  i = 2
+  largest = 0
+  while (i <= n)
+    if (n % i == 0)
+      while (n % i == 0 )
+        n = n / i
+        largest = i
+        i += 1
+      end
+    end
+    i += 1
+  end
+  return largest
+end
+puts lPrimeFactor(52000)
